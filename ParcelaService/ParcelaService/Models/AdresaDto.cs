0d95567bbd096c628f8d46fe124f37b97db7bd27
Milan Brčkalo <mbrckalo997@outlook.com>
2022-01-28 17:00:02 +0100

    Dodavanje mock repozitorijuma za Lice

---
+++ ParcelaService/ParcelaService/Models/AdresaDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ParcelaService.Models
+{
+    public class AdresaDto
+    {
+        public Guid AdresaID { get; set; }
+        public string Ulica { get; set; }
+        public string Broj { get; set; }
+        public string Mesto { get; set; }
+        public string PostanskiBroj { get; set; }
+        public DrzavaDto Drzava { get; set; }
+    }
+}
---
+++ ParcelaService/ParcelaService/Models/DrzavaDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParcelaService.Models
+{
+    public class DrzavaDto
+    {
+        public Guid DrzavaID { get; set; }
+        public string NazivDrzave { get; set; }
+    }
+}
