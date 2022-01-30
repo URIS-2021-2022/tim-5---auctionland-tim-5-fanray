28b8b4dca77dcb8123a8e9a4b61407f3aba24ba4
Milan Brčkalo <mbrckalo997@outlook.com>
2022-01-30 19:19:43 +0100

    Dodat Logger servis

---
+++ LoggerService/LoggerService/Entities/Log.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace LoggerService.Entities
+{
+    public class Log
+    {
+        [Key]
+        public Guid LogID { get; set; }
+
+        [Required]
+        public string Opis { get; set; }
+
+        [Required]
+        public string DatumKreiranja { get; set; }
+        
+        [Required]
+        public string VremeKreiranja { get; set; }
+    }
+}
---
+++ LoggerService/LoggerService/Models/LogDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoggerService.Models
+{
+    public class LogDto
+    {
+        public Guid LogID { get; set; }
+        public string Opis { get; set; }
+        public string DatumKreiranja { get; set; }
+        public string VremeKreiranja { get; set; }
+    }
+}
