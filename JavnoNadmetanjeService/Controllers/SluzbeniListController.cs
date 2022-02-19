4ef7cee16dc5cf97335f96debb6cb2a3a665994c
Milan Brčkalo <mbrckalo997@outlook.com>
2022-02-19 11:19:40 +0100

    CS0105

--- JavnoNadmetanjeService/Controllers/SluzbeniListController.cs
+++ JavnoNadmetanjeService/Controllers/SluzbeniListController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
 using System;
